102e4acac4ffc3ac8a5aaabcd85354aeb36a7365
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2021-12-10 13:20:45 -0300

    criacao do login

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div> App react</div>;
+  return (
+    <div>
+      {/* Configuração das rotas (Tem que ter o browserRouter por volta de tudo) */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route> {/*Rota para a home. */}
+          <Route path="/login/*" element={<Login />}></Route>{' '}
+          {/*Rota para o Login. */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
