77bcc8ca146c2cdf8ea02b00869dfa735cf9d38b
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2022-01-05 11:49:33 -0300

    loading image complete

--- dogs/src/Components/Feed/FeedPhotosItem.js
+++ dogs/src/Components/Feed/FeedPhotosItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './FeedPhotositem.module.css';
+import Image from '../Helper/Image';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
@@ -7,7 +8,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title}></img>
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
